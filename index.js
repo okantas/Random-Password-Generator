444b85e928220c89a447c97a4f3e34065c98d2a8
okantas <okantas954@gmail.com>
2023-02-05 15:52:37 +0300

    I am gonna start to js part

---
+++ index.js
@@ -0,0 +1,10 @@
+
+let firstPassword=document.getElementById("first_password")
+let secondPassword=document.getElementById("second_password")
+
+
+addEventListener("click", function(){
+    
+
+
+})
